57871edc1ae16c98ccf3502b08a1ac848f24bbe8
Tom King <design.tomking@gmail.com>
2018-11-20 11:53:47 -0700

    Create RandomDie and methods

--- server.js
+++ server.js
@@ -4,15 +4,53 @@ const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!,
+    rollOnce: Int!,
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    hello: String
+    quoteOfTheDay: String,
+    random: Float!,
+    rollDice(numDice: Int!, numSides: Int): [Int],
+    getDie(numSides: Int): RandomDie
   }
 `);
 
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+  roll({numRolls}) {
+    let totalRolls = [];
+    for (let i=0; i<numRolls; i++) {
+      totalRolls.push(this.rollOnce());
+    }
+    return totalRolls;
+  }
+}
+
 const root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
+  random: () => {
+    return Math.random();
+  },
+  rollDice: function ({numDice, numSides}) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6);
+  }
 };
 
 app.use('/graphql', graphqlHTTP({
