a09810eaab8789d27c316c56bd17e4e6fbea6e53
Tom King <design.tomking@gmail.com>
2018-11-20 10:52:03 -0700

    Initial commit add server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000)
+console.log('Hey, Listen! Running GraphQL server at localhost:4000/graphql')
